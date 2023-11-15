6b27c852c112782a3010913e8fdd483b51ad9487
BrustS <69087483+BrustS@users.noreply.github.com>
2023-11-15 22:56:46 +0300

    solusion task 7

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,33 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+
+val textId = "0"
+var textString = "Hello World!";
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(textId, textString)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        textString =  savedInstanceState.getString(textId) ?: ""
+        super.onRestoreInstanceState(savedInstanceState)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.changeButton)
+        text.text = textString
+        button.setOnClickListener {
+            textString = resources.getString(R.string.changedtext)
+            text.text = textString
+        }
     }
 }

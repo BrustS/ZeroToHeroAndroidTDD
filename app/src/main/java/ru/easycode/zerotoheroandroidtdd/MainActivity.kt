87f4934ebfc761643625d9d56e053c5aa1af704c
BrustS <69087483+BrustS@users.noreply.github.com>
2023-11-16 21:38:12 +0300

    solusion task 10

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,44 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+
+val textId = ""
+var vision = 0
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(textId, vision)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        vision = savedInstanceState.getInt(textId)
+    }
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.removeButton)
+        val textview = findViewById<TextView>(R.id.titleTextView)
+        val linear = findViewById<LinearLayout>(R.id.rootLayout)
+        if (vision == 1) {
+            linear.removeView(textview)
+            button.isEnabled = false
+        }
+
+        button.setOnClickListener {
+            linear.removeView(textview)
+            button.isEnabled = false
+            vision = 1
+        }
+
     }
 }
